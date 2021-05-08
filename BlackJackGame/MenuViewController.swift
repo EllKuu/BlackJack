47d0b2b0c1f3b3d8d1d1deb142853d00cb931a32
elliott kung <elliott7506@rogers.com>
2021-05-08 15:04:35 -0400

    rearranged stackview layout

--- BlackJackGame/MenuViewController.swift
+++ BlackJackGame/MenuViewController.swift
@@ -35,10 +35,22 @@ class MenuViewController: UIViewController {
     var playButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.setTitle("Play", for: .normal)
-        btn.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        btn.titleLabel?.font = .boldSystemFont(ofSize: 30)
         btn.backgroundColor = .white
         btn.addTarget(self, action: #selector(startGame), for: .touchUpInside)
-        btn.layer.cornerRadius = 5
+        btn.layer.cornerRadius = 50
+        btn.layer.borderWidth = 1
+        
+        return btn
+    }()
+    
+    var savedGamesButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Saved Games", for: .normal)
+        btn.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        btn.backgroundColor = .white
+        btn.addTarget(self, action: #selector(savedGames), for: .touchUpInside)
+        btn.layer.cornerRadius = 50
         btn.layer.borderWidth = 1
         
         return btn
@@ -47,8 +59,8 @@ class MenuViewController: UIViewController {
     var numOfDecksImage: UIImageView = {
         let deckImage = UIImageView()
         deckImage.image = UIImage(systemName: "1.square")
-        deckImage.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        deckImage.contentMode = .scaleAspectFill
+        deckImage.frame = CGRect(x: 0, y: 0, width: 25, height: 25)
+        deckImage.contentMode = .scaleAspectFit
         
         
         return deckImage
@@ -59,7 +71,6 @@ class MenuViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .green
         self.title = "BlackJack"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Saved Games", style: .plain, target: self, action: #selector(savedGames))
         
         setupMenuLayout()
     }
@@ -67,32 +78,22 @@ class MenuViewController: UIViewController {
     /// shuffles deck of cards player can choose how many decks 1-8
     func setupMenuLayout(){
         
-        let menuStackView = UIStackView(arrangedSubviews: [textLabel, numOfDecksImage, deckSlider, playButton])
+        let menuStackView = UIStackView(arrangedSubviews: [textLabel, numOfDecksImage, savedGamesButton ,playButton, deckSlider])
         menuStackView.axis = .vertical
         menuStackView.distribution = .fillEqually
-        menuStackView.spacing = 100
+        menuStackView.alignment = .fill
+        menuStackView.spacing = 25
+        //menuStackView.frame = view.bounds.insetBy(dx: 10, dy: 100)
+        //menuStackView.backgroundColor = .systemRed
         
         view.addSubview(menuStackView)
         
         menuStackView.translatesAutoresizingMaskIntoConstraints = false
-        menuStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        menuStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
         menuStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         menuStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        menuStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        menuStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
         
-        menuStackView.frame = CGRect(x: 0, y: 0, width: 200, height: 500)
-        
-        
-
-        
-//        textLabel.frame = CGRect(x: 0, y: 100, width: view.frame.width, height: 50)
-//        deckSlider.frame = CGRect(x: 0, y: 150, width: view.frame.width, height: 50)
-//        playButton.frame = CGRect(x: 0, y: 200, width: view.frame.width, height: 50)
-        
-        
-//        view.addSubview(textLabel)
-//        view.addSubview(deckSlider)
-//        view.addSubview(playButton)
         
     }
     
