9a71cfd414ac44166b82cca81238ac21d5c0fadb
elliott kung <elliott7506@rogers.com>
2021-06-01 15:27:24 -0400

    chnages to menu VC layout of image and buttons

--- BlackJackGame/MenuViewController.swift
+++ BlackJackGame/MenuViewController.swift
@@ -8,9 +8,6 @@
 import UIKit
 
 class MenuViewController: UIViewController {
-
-    var numberOfDecks = 1
-    
     
     var playButton: UIButton = {
         let btn = UIButton(type: .system)
@@ -18,7 +15,7 @@ class MenuViewController: UIViewController {
         btn.titleLabel?.font = .boldSystemFont(ofSize: 30)
         btn.backgroundColor = .white
         btn.addTarget(self, action: #selector(startGame), for: .touchUpInside)
-        btn.layer.cornerRadius = 50
+        btn.layer.cornerRadius = 15
         btn.layer.borderWidth = 1
         btn.layer.shadowOpacity = 0.5
         btn.layer.shadowOffset = CGSize(width: 5, height: 5)
@@ -32,23 +29,21 @@ class MenuViewController: UIViewController {
         btn.titleLabel?.font = .boldSystemFont(ofSize: 30)
         btn.backgroundColor = .white
         btn.addTarget(self, action: #selector(savedGames), for: .touchUpInside)
-        btn.layer.cornerRadius = 50
+        btn.layer.cornerRadius = 15
         btn.layer.borderWidth = 1
         btn.layer.shadowOpacity = 0.5
         btn.layer.shadowOffset = CGSize(width: 5, height: 5)
-        
+       
         return btn
     }()
     
     var logoImage: UIImageView = {
-        let deckImage = UIImageView()
-        deckImage.image = UIImage(named: "LogoImage")
-        deckImage.frame = CGRect(x: 0, y: 0, width: 25, height: 25)
-        deckImage.contentMode = .scaleAspectFit
-        deckImage.tintColor = .black
+        let image = UIImageView()
+        image.image = UIImage(named: "LogoImage")
+        image.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        image.contentMode = .scaleAspectFit
         
-        
-        return deckImage
+        return image
     }()
         
     override func loadView() {
@@ -81,23 +76,53 @@ class MenuViewController: UIViewController {
     /// shuffles deck of cards player can choose how many decks 1-8
     func setupMenuLayout(){
         
-        let menuStackView = UIStackView(arrangedSubviews: [ logoImage, savedGamesButton ,playButton])
-        menuStackView.axis = .vertical
-        menuStackView.distribution = .fillEqually
-        menuStackView.alignment = .fill
-        menuStackView.spacing = 25
-        //menuStackView.frame = view.bounds.insetBy(dx: 10, dy: 100)
-        //menuStackView.backgroundColor = .systemRed
+        view.addSubview(logoImage)
+        view.addSubview(savedGamesButton)
+        view.addSubview(playButton)
         
-        view.addSubview(menuStackView)
+        setupMenuLayoutConstraints()
         
-        menuStackView.translatesAutoresizingMaskIntoConstraints = false
-        menuStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-        menuStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        menuStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        menuStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+//        let menuStackView = UIStackView(arrangedSubviews: [ logoImage, savedGamesButton ,playButton])
+//        menuStackView.axis = .vertical
+//        menuStackView.distribution = .fillEqually
+//        menuStackView.alignment = .fill
+//        menuStackView.spacing = 25
+//        view.addSubview(menuStackView)
+//
+//        menuStackView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        menuStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+//        menuStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+//        menuStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+//        menuStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
         
         
+    }
+    
+    private func setupMenuLayoutConstraints(){
+        
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        savedGamesButton.translatesAutoresizingMaskIntoConstraints = false
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+        
+            logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            logoImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            logoImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
+            logoImage.heightAnchor.constraint(equalToConstant: 250),
+            
+            savedGamesButton.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 50),
+            savedGamesButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            savedGamesButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
+            
+            playButton.topAnchor.constraint(equalTo: savedGamesButton.bottomAnchor, constant: 50),
+            playButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            playButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
+            
+            
+            
+        ])
     }
     
     @objc func startGame(){
